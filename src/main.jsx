1f44444c3b13c0a8f7b0a528aa4b06bf35cdc118
unknown <oviyaarivazhagan.ara@gmail.com>
2025-12-04 10:52:58 +0530

    Ara-tech_Latest_0412

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Ara-Tech_Latest/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
